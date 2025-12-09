84e61762c7b3b98fa5f2d1197e3f30c910e59fad
Hanna Torianyk <an.mol4anova@gmail.com>
2025-12-09 12:21:31 +0200

    Fixed the code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let price = days * 40;
+  const PRICE_PER_DAY = 40;
+  const LONG_PERIOD = 7;
+  const LONG_PERIOD_DISCOUNT = 50;
+  const SHORT_PERIOD = 3;
+  const SHORT_PERIOD_DISCOUNT = 20;
+  let price = days * PRICE_PER_DAY;
 
-  if (days >= 7) {
-    price -= 50;
-  } else if (days >= 3) {
-    price -= 20;
+  if (days >= LONG_PERIOD) {
+    price -= LONG_PERIOD_DISCOUNT;
+  } else if (days >= SHORT_PERIOD) {
+    price -= SHORT_PERIOD_DISCOUNT;
   }
 
   return price;
